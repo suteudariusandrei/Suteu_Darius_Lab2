fbb2df89b3eb483d913235fd9b8044a15c33a0a7
Suteu Darius <dariussuteu9@gmail.com>
2024-10-27 16:53:10 +0200

    lab 3 rezolvat

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Suteu_Darius_Lab2.Data;
@@ -12,18 +9,18 @@ namespace Suteu_Darius_Lab2.Pages.Categories
 {
     public class IndexModel : PageModel
     {
-        private readonly Suteu_Darius_Lab2.Data.Suteu_Darius_Lab2Context _context;
+        private readonly Suteu_Darius_Lab2Context _context;
 
-        public IndexModel(Suteu_Darius_Lab2.Data.Suteu_Darius_Lab2Context context)
+        public IndexModel(Suteu_Darius_Lab2Context context)
         {
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
+        public IList<Category> Categories { get; set; } = new List<Category>();
 
         public async Task OnGetAsync()
         {
-            Category = await _context.Category.ToListAsync();
+            Categories = await _context.Category.ToListAsync();
         }
     }
 }
