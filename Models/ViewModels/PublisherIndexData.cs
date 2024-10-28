555cd434b5c9fcc603d6cab83a3e5467fc72edc9
Suteu Darius <dariussuteu9@gmail.com>
2024-10-28 18:13:08 +0200

    edit

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Suteu_Darius_Lab2.Models;
+namespace Suteu_Darius_Lab2.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -21,9 +21,24 @@ namespace Suteu_Darius_Lab2.Pages.Publishers
 
         public IList<Publisher> Publisher { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Publisher = await _context.Publisher.ToListAsync();
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+            .Include(i => i.Books)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.PublisherName)
+            .ToListAsync();
+            if (id != null)
+            {
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
+            }
         }
     }
 }
