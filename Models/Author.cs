a4b4ba3c3d82dc19b127e4aee5fa38b4675d2cf1
Suteu Darius <dariussuteu9@gmail.com>
2024-10-28 18:22:00 +0200

    .

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,20 @@
-﻿namespace Suteu_Darius_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Suteu_Darius_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; } 
         public string? FirstName { get; set; }  
-        public string? LastName { get; set; }   
+        public string? LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
