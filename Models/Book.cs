fd3116cdc1492e2018bf3b8edd74116606e9d336
Suteu Darius <dariussuteu9@gmail.com>
2024-11-04 18:18:44 +0200

    .

--- Models/Book.cs
+++ Models/Book.cs
@@ -17,6 +17,8 @@ namespace Suteu_Darius_Lab2.Models
         public Publisher? Publisher { get; set; }
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
+        public ICollection<Borrowing>? Borrowings { get; set; }
+
         public ICollection<BookCategory>? BookCategories { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Suteu_Darius_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Suteu_Darius_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Suteu_Darius_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Suteu_Darius_Lab2Context' not found.")));
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 
+options.UseSqlServer(builder.Configuration.GetConnectionString("Suteu_Darius_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Suteu_Darius_Lab2Context' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
