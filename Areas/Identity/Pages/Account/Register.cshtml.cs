bbf1e280e705d93b2a6a386aaefe63f87f5b9f1a
Suteu Darius <dariussuteu9@gmail.com>
2024-11-17 16:01:49 +0200

    commit

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -135,6 +135,7 @@ namespace Suteu_Darius_Lab2.Areas.Identity.Pages.Account
             if (result.Succeeded)
             {
                _logger.LogInformation("User created a new account with password.");
+                var role = await _userManager.AddToRoleAsync(user, "User");
                 var userId = await _userManager.GetUserIdAsync(user);
                 var code = await
                _userManager.GenerateEmailConfirmationTokenAsync(user);
--- Program.cs
+++ Program.cs
@@ -5,13 +5,19 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+});
 builder.Services.AddDbContext<Suteu_Darius_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Suteu_Darius_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Suteu_Darius_Lab2Context' not found.")));
 builder.Services.AddDbContext<LibraryIdentityContext>(options =>
 
 options.UseSqlServer(builder.Configuration.GetConnectionString("Suteu_Darius_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Suteu_Darius_Lab2Context' not found.")));
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+     .AddRoles<IdentityRole>()
  .AddEntityFrameworkStores<LibraryIdentityContext>();
 var app = builder.Build();
 
